afb5275415c20eb92b02b9085371996e4d555229
Ryan Villanueva <ravencoder@users.noreply.github.com>
2016-07-01 16:25:36 +0800

    Initial Commit

---
+++ RoyalCopenhagen/Catalog/Block/ProductListing.php
@@ -0,0 +1,14 @@
+<?php
+namespace RoyalCopenhagen\Catalog\Block;
+
+/**
+* ProductListing block
+*/
+class ProductListing
+    extends \Magento\Framework\View\Element\Template
+{
+    public function getTitle()
+    {
+        return "RoyalCopenhagen Catalog Block";
+    }
+}
---
+++ RoyalCopenhagen/Catalog/registration.php
@@ -0,0 +1,7 @@
+<?php
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'RoyalCopenhagen_Catalog',
+    __DIR__
+);
