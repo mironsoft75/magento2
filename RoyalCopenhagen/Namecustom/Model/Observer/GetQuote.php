751d41639e693e0a4c234330c3107b157e6896fc
Ryan L Villanueva <raven.0707@gmail.com>
2016-07-11 17:12:32 +0800

    Updated logger declaration

--- RoyalCopenhagen/Namecustom/Model/Observer/GetQuote.php
+++ RoyalCopenhagen/Namecustom/Model/Observer/GetQuote.php
@@ -4,17 +4,11 @@ namespace RoyalCopenhagen\Namecustom\Model\Observer;
 
 use Magento\Framework\Event\ObserverInterface;
 
-protected $logger;
-
 class GetQuote implements ObserverInterface {
-
-  public function __construct (\Psr\Log\LoggerInterface $logger) {
-    $this->logger = $logger;
-  }
 
   public function execute(Observer $observer) {
     $event = $observer->getEvent();
     $quote = $event->getQuote();
-    $this->logger->debug('something');
+    $this->_objectManager->get('Psr\Log\LoggerInterface')->debug('something');
   }
 }
